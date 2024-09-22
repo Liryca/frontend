3de12a3115313bcb6ab34d7566a2ee1e934b9082
Tarasova Anna <tarasavahanna@gmail.com>
2024-09-22 16:18:57 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import UserTable from "./components/UserTable";
-import { useAuth } from "./context/AuthProvider";
 import "./App.css";
 
 function App() {
-  const { user } = useAuth();
-  console.log(user);
-  console.log(localStorage.getItem("token"));
   return (
     <div className="container">
       <Router>
